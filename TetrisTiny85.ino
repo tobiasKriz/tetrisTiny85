3e3e0c31fafb20f9bf51f65717740f40554f1034
Tobiáš Križanský <61013055+tobiasKriz@users.noreply.github.com>
2025-01-23 16:56:47 +0100

    Update TetrisTiny85.ino

--- TetrisTiny85.ino
+++ TetrisTiny85.ino
@@ -1,35 +1,14 @@
-/*
-   Tiny4kOLED - Drivers for SSD1306 controlled dot matrix OLED/PLED 128x32 displays
 
-   Based on ssd1306xled, re-written and extended by Stephen Denne
-   from 2017-04-25 at https://github.com/datacute/Tiny4kOLED
-
-*/
-
-// Choose your I2C implementation before including Tiny4kOLED.h
-// The default is selected is Wire.h
-
-// To use the Wire library:
-//#include <Wire.h>
-
-// To use the Adafruit's TinyWireM library:
 #include <TinyWireM.h>
 
-// To use the TinyI2C library from https://github.com/technoblogy/tiny-i2c
-//#include <TinyI2CMaster.h>
-
-// The blue OLED screen requires a long initialization on power on.
-// The code to wait for it to be ready uses 20 bytes of program storage space
-// If you are using a white OLED, this can be reclaimed by uncommenting
-// the following line (before including Tiny4kOLED.h):
 #define TINY4KOLED_QUICK_BEGIN
 
 #include <Tiny4kOLED.h>
-//#include "SolomonSystech.h"
+
 
 // ============================================================================
 bool block1[4][3] = {
-  {0, 0, },
+  {0, 0, 0},
   {0, 0, 0},
   {1, 0, 0},
   {1, 1, 1}
@@ -60,7 +39,7 @@ int temp = 0;
 int cursorX = 4;
 int cursorY = 8;
 void setup() {
-  // put your setup code here, to run once:
+
 
   oled.begin(128, 64, sizeof(tiny4koled_init_128x64r), tiny4koled_init_128x64r);
  
